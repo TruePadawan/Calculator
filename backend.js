72696e3d9c378d95fa0eb3b4859a3405bb1e0029
Hermes <hermeschigoziri@gmail.com>
2021-12-27 14:08:06 +0100

    Improve calc logic

--- backend.js
+++ backend.js
@@ -1,15 +1,24 @@
 const ADDITION_OPERATOR = '+';
 const SUBTRACTION_OPERATOR = '-';
-const MULTIPLICATION_OPERATOR = '*';
+const MULTIPLICATION_OPERATOR = 'x';
 const DIVISION_OPERATOR = '/';
+const EQUALITY_OPERATOR = '=';
 
 let result = document.querySelector('#result');
 let calculations = document.querySelector('#calculations');
+
 let clearButton = document.querySelector('#clearButton');
 let deleteButton = document.querySelector('#deleteButton');
-let nonSpecialButtons = document.querySelectorAll('.button-row button:not(.special-button, .extra-special)');
-let decimalButton = document.querySelector('.extra-special');
 
+let nonSpecialButtons = document.querySelectorAll('.button-row button:not(.special-button, .extra-special)');
+let specialButtons = document.querySelectorAll('.special-button:not(#equality)');
+let decimalButton = document.querySelector('.extra-special');
+let equalityButton = document.querySelector('#equality');
+
+let currentOperator = '';
+let secondOperand = false;
+
+// FUNCTIONS USED IN CALC LOGIC
 const Add = (a, b) => Number(a) + Number(b);
 const Subtract = (a, b) => Number(a) - Number(b);
 const Multiply = (a, b) => Number(a) * Number(b);
@@ -22,6 +31,34 @@ const operate = (operator,a,b) => {
     if (operator === DIVISION_OPERATOR) return Divide(a, b);
 }
 
+const needsTobeEvaluated = () => {
+    if (calculations.textContent.includes(ADDITION_OPERATOR)) return true;
+    if (calculations.textContent.includes(SUBTRACTION_OPERATOR)) return true;
+    if (calculations.textContent.includes(MULTIPLICATION_OPERATOR)) return true;
+    if (calculations.textContent.includes(DIVISION_OPERATOR)) return true;
+    if (calculations.textContent.includes(EQUALITY_OPERATOR)) return true;
+    return false;
+}
+
+const getOperands = (operator) => {
+    let operands = calculations.textContent.split(operator);
+    return {
+        a: operands[0],
+        b: operands[1],
+        operator
+    };
+}
+
+const calc = (data) => {
+    let res = operate(data.operator,data.a,data.b);
+    if (res.toString().length > 4)
+    {
+        result.textContent = res.toFixed(3);
+        return;
+    }
+    result.textContent = res;
+}
+/////////////////////////////
 // EVENT LISTENERS FOR BUTTONS
 clearButton.addEventListener('click', () => {
     result.textContent = 0;
@@ -32,8 +69,12 @@ deleteButton.addEventListener('click', () => {
     if (result.textContent.length > 1)
     {
         result.textContent = result.textContent.substring(0, result.textContent.length - 1);
+    }else if (result.textContent.length === 1 && result.textContent != '0')
+    {
+        result.textContent = '0';
     }
 });
+
 nonSpecialButtons.forEach((button) => {
     button.addEventListener('click', () => {
         if (result.textContent === '0')
@@ -49,6 +90,24 @@ nonSpecialButtons.forEach((button) => {
             }
         }
         console.log(`Button ${button.textContent} clicked!`);
+    })
+});
+
+specialButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (secondOperand)
+        {
+            let newRes = calculations.textContent + result.textContent;
+            calculations.textContent = newRes;
+            let operands = getOperands(currentOperator);
+            calc(operands);
+        }
+        
+        currentOperator = button.textContent;
+        let newRes = result.textContent + currentOperator;
+        calculations.textContent = newRes;
+        result.textContent = '0';
+        secondOperand = true;
     })
 });
 
